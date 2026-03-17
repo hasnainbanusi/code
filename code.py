e7d4ea5d22ae3e8c9475dab2e4f39cddeda95646
Hasnain Banusi <your-github-hasnain10.official@gmail.com>
2026-03-17 23:40:30 +0500

    first commit

--- code.py
+++ code.py
@@ -1,1 +1,7 @@
 print("hello world this is me here again  after a long time break where i was learninglinux basics again to get back ")
+
+def hello():
+    print("this is my first function in python")
+
+
+hello()
